d892365ac6e62d48e3fb170ba3c54ff1f6f88775
xiraynedev <xiraynedev@gmail.com>
2022-02-19 14:58:16 -0500

    initial commit with webpack setup

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,10 @@
+import {FC} from 'react';
+import {Login} from '../Login/Login';
+
+export const Container: FC = () => {
+  return (
+    <div className="container">
+     <Login />
+    </div>
+  );
+};
