863d1f513a02a9403083bc7798574e20c0f03837
xiraynedev <xiraynedev@gmail.com>
2022-09-21 23:15:38 -0400

    remove unused import

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import { applyMiddleware, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import {reducers} from './reducers';
 
